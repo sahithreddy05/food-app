afe9a9eff341cdc620f3ee9c35bf77b3beb848a6
sri sahith reddy kuncharam <sahith@sris-MacBook-Pro.local>
2024-01-22 22:36:35 +0530

    update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
 "https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png"
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  `https://api.allorigins.win/raw?url=${encodeURIComponent('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=')}`;
